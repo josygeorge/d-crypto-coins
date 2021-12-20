204c83c875bad4285ee2b86756b5e73adb12f8bf
Josy George <joe.george.josy@gmail.com>
2021-12-20 04:21:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+const App = () => {
+    return (
+        <div className='app'>
+            <h1>D-Crypto-Coin</h1>
+            <div className='navbar menu header'>
+
+            </div>
+            <div className='content body-section'>
+
+            </div>
+            <div className='footer-section'>
+
+            </div>
+
+        </div>
+    )
+}
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { Avatar, Button, Menu, Typography } from 'antd';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BulbOutlined, FundOutlined, HomeOutlined, MenuOutlined, MoneyCollectOutlined } from '@ant-design/icons';
+
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Navbar;
